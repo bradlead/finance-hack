80331628153dc468a76e92d34af6981b7b46b640
AbhinavJ1862 <abhi.jayanty@outlook.com>
2019-06-22 19:31:50 +0100

    Add files via upload

---
+++ BTU-hack/script.js
@@ -0,0 +1,15 @@
+CK.connect({
+    client_id: '487cba89a4742c4ec3955dccfb7ae125c07f02132ca4b7c66722d9a2fd5c2b43',
+    redirect_uri: 'https://localhost:5500/landing.html',
+    customer_token: 'YOUR_CUSTOMER_TOKEN',
+    selector: '#container',
+    state: {
+        userId: '12345',
+    }
+})
+    .then(function (params) {
+        // your success handling code here
+    })
+    .catch(function (params) {
+        // your error handling code here
+    });
